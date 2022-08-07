0e8e9cfdd47bdcebcd7be9fa9e1e9e49b381e235
vzen2901 <vzen2901@gmail.com>
2022-08-07 16:22:00 +0700

    fdfds

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import React from 'react';
+// import {Component} from 'react'
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//       </header>
+//       <h1>hi, i'm a react app</h1>
+//     </div>
+//   );
+// }
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <h1>hi, i'm a react app</h1>
-    </div>
-  );
+  return React.createElement('div',{className: App}, React.createElement('h1',null,'i am react app'));
 }
 
+// class App extends Component {
+//   render(){
+//     return React.createElement('div', {className: App}, React.createElement('h1',null,'i am react app'));
+//   }
+// }
+
+
 export default App;
+
+
