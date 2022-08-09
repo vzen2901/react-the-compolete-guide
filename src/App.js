278ddb848bdb9d46e8cda7ea49be7a7567d334e7
vzen2901 <vzen2901@gmail.com>
2022-08-09 11:29:33 +0700

    gdgdf

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 // import {Component} from 'react'
 import './App.css';
+import Person from './Person/Person';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//       </header>
-//       <h1>hi, i'm a react app</h1>
-//     </div>
-//   );
-// }
 
 function App() {
-  return React.createElement('div',{className: App}, React.createElement('h1',null,'i am react app'));
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <h1>hi, i'm a react app</h1>
+      <Person name = "max" age = "29">nọi dung</Person>
+    </div>
+  );
 }
+
+// function App() {
+//   return React.createElement('div',{className: App}, React.createElement('h1',null,'i am react app'));
+// }
 
 // class App extends Component {
 //   render(){
---
+++ src/Person/Person.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+const Person = (props) => {
+    return (
+        <div>
+            <p>i am {props.name}, age {props.age}</p>
+            <p>{props.children}</p>
+        </div>
+
+    );
+}
+export default Person;
