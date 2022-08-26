42b7a11128bc549a7f02d5246d24715f69dbf2ab
vzen2901 <vzen2901@gmail.com>
2022-08-26 20:36:48 +0700

    hhhh

--- src/componets/Persons/Person/Person.js
+++ src/componets/Persons/Person/Person.js
@@ -1,23 +1,29 @@
 
+// import React from "react";
+// import './Person.css';
+
+// import Radium from "radium";
+
+
+// const Person = (props) => {
+//     const style = {
+//         '@media (min-Width: 500px)': {
+//                 width: '450px',
+//                 border: '1px solid #FF0000'
+//         }
+//     };
+//     return (
+//             <div className="Person" style={style}>
+//                 <p onClick={props.click}>i am {props.name} and i am years old {props.age}</p>
+//                 {/* <p onClick={props.switchNameAge}>{props.children}</p> */}
+//                 <input type="text" onChange={props.namehange} defaultValue = {props.name}/>
+//             </div>
+//     );
+// }
+// export default Radium(Person);
 import React from "react";
 import './Person.css';
 
-import Radium from "radium";
-
-
 const Person = (props) => {
-    const style = {
-        '@media (min-Width: 500px)': {
-                width: '450px',
-                border: '1px solid #FF0000'
-        }
-    };
-    return (
-            <div className="Person" style={style}>
-                <p onClick={props.click}>i am {props.name} and i am years old {props.age}</p>
-                {/* <p onClick={props.switchNameAge}>{props.children}</p> */}
-                <input type="text" onChange={props.namehange} defaultValue = {props.name}/>
-            </div>
-    );
-}
-export default Radium(Person);
+    return ();
+};
