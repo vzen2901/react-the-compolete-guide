f46816b68e6d70e7366dc000c9fa0e8ea94d5304
vzen2901 <vzen2901@gmail.com>
2022-08-28 20:33:38 +0700

    llll

--- src/componets/Persons/Person/Person.js
+++ src/componets/Persons/Person/Person.js
@@ -1,37 +1,20 @@
-
-// import React from "react";
-// import './Person.css';
-
-// import Radium from "radium";
-
-
-// const Person = (props) => {
-//     const style = {
-//         '@media (min-Width: 500px)': {
-//                 width: '450px',
-//                 border: '1px solid #FF0000'
-//         }
-//     };
-//     return (
-//             <div className="Person" style={style}>
-//                 <p onClick={props.click}>i am {props.name} and i am years old {props.age}</p>
-//                 {/* <p onClick={props.switchNameAge}>{props.children}</p> */}
-//                 <input type="text" onChange={props.namehange} defaultValue = {props.name}/>
-//             </div>
-//     );
-// }
-// export default Radium(Person);
-import React from "react";
+import React, {Component} from "react";
 import './Person.css';
 
-const Person = (props) => props.persons.map((person, index) => {  
-    console.log("personjs rendering...");
-    return (
-            <div className="Person" key = {person.id}>
-                <p onClick={() => props.clicked(index)}>i am {person.name} and i am years old {person.age}</p>
-                <input type="text" onChange={(event)=>props.Changed(event, person.id)} defaultValue = {person.name}/>
-            </div>
-    );
-});
+class Person extends Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return this.props.persons.map((person, index) => { 
+            return(
+                <div className="Person" key = {person.id}>
+                    <p onClick={() => this.props.clicked(index)}>i am {person.name} and i am years old {person.age}</p>
+                    <input type="text" onChange={(event)=>this.props.Changed(event, person.id)} defaultValue = {person.name}/>
+                </div>
+            );
+        });
+    }
+}
 
 export default Person;
