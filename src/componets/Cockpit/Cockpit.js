1f4896a1c1c942cc094d9bfcb39a9d69f8961b4e
vzen2901 <vzen2901@gmail.com>
2022-08-30 21:24:32 +0700

    kk

--- src/componets/Cockpit/Cockpit.js
+++ src/componets/Cockpit/Cockpit.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { useEffect } from "react";
 const Cockpit = (props) => {
+    useEffect(() => {
+        console.log("cockpit.js useEffect");
+        //http request...
+    });
     return (
         <div>
             <p>This is really working!</p>
